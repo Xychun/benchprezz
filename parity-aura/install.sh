3fc0b7d8371318656378de330d28f592dc3143cc
Xychun <senju@hotmail.de>
2019-09-27 12:17:18 +0200

    fixed install script for clients

--- parity-aura/install.sh
+++ parity-aura/install.sh
@@ -2,10 +2,18 @@
 sudo apt-get update
 # install dependencies to build the source files
 sudo apt-get install build-essential cmake libudev-dev
+sudo apt-get install libtool
+sudo apt-get install autoconf
+sudo apt-get install libcurl4-gnutls-dev
 sudo apt-get install make
 # install parity
 bash <(curl https://get.parity.io -L)
+# install restlcient-cpp
+git clone https://github.com/mrtazz/restclient-cpp.git
+cd restclient-cpp/ && ./autogen.sh && ./configure && sudo make install
+cd ..
+sudo rm -r restclient-cpp/
 # make for client driver
-#cd benchprezz/parity-aura/src/macro/kvstore/
-#make
-#cd ~
+cd benchprezz/parity-aura/src/macro/kvstore/
+make
+cd ~
