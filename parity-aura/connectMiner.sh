6a76a82615fe2a3f4bad54b44a4bfa37b8fa218c
Xychun <senju@hotmail.de>
2019-09-27 12:08:19 +0200

    fixed connecting peers

--- parity-aura/connectMiner.sh
+++ parity-aura/connectMiner.sh
@@ -8,8 +8,7 @@ rpcport=$2
 for peer in `cat $AURA_HOME/peers`; do
   if [[ $i -lt $minerCount ]]; then
     echo adding peer $peer
-    curl --data '{"jsonrpc":"2.0","method":"parity_addReservedPeer","params":["${peer}"],"id":0}' -H "Content-Type: application/json" -X POST localhost:$rpcport
-    # curl --data '{"jsonrpc":"2.0","method":"parity_addReservedPeer","params":["enode:${peer}"],"id":0}' -H "Content-Type: application/json" -X POST localhost:$rpcport
+    curl --data '{"jsonrpc":"2.0","method":"parity_addReservedPeer","params":['$peer'],"id":0}' -H "Content-Type: application/json" -X POST localhost:$rpcport
   fi
     let i=$i+1
 done
