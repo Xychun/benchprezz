d7b890724ab78ecc34a8b4c95e77c238de9b295d
Xychun <senju@hotmail.de>
2019-10-09 14:35:51 +0200

    modified file permission; added more nodes

--- geth-clique/install.sh
+++ geth-clique/install.sh
@@ -10,7 +10,7 @@ sudo apt-get install make
 curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
 sudo apt-get install nodejs
 sudo apt-get install npm
-cd ..
+cd ~/benchprezz/
 npm install
 # fix smartcard socket bug
 sudo apt-get install pcscd
