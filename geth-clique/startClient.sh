7957621881c8d008fdb89701bc1a8c6c242575b5
Xychun <senju@hotmail.de>
2019-10-09 12:06:56 +0200

    adapted get-clique to new benchmark script

--- geth-clique/startClient.sh
+++ geth-clique/startClient.sh
@@ -4,20 +4,17 @@ cd `dirname ${BASH_SOURCE-$0}`
 
 minerCount=$1
 clientCount=$2
-threadCount=$3
-txrate=$4
-clientId=$5
+txrate=$3
+txLimit=$4
+let clientId=$5
+
+let deployTime=10
 
 timestamp=$(date +"%Y-%m-%d_%H-%M-%S")
 log_dir=$LOG_DIR/$timestamp"_"$1"_"miners_$2"_"clients_$3"_"threads_$4"_"txrate
 mkdir -p $log_dir
 
-i=0
-echo "iterating over miners for clientID=$5"
-for miner in `cat $MINERS`; do
-  cd $DRIVER_HOME
-  rpcport=`expr $RPCPORT_INIT + $i`
-  echo "Starting driver for endpoint " $miner:$rpcport
-  nohup ./driver -db ethereum -threads $threadCount -P workloads/workloada.spec -endpoint $miner:$rpcport -txrate $txrate -wt 60 > $log_dir/client_$clientId"_"$miner 2>&1 &
-  let i=i+1
-done
+cd $BENCHMARK_HOME
+rpcport=`expr $RPCPORT_INIT + $clientId`
+echo "Starting clientID" $5 " for endpoint " $miner:$rpcport
+nohup node ./run.js $miner:$rpcport "KVStore" $deployTime $txrate $txLimit  > $log_dir/client_$clientId"_"$miner 2>&1 &
