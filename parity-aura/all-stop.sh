7901d11501e5aa2a38643757e3230264bbc94774
Xychun <senju@hotmail.de>
2019-10-10 13:42:58 +0200

    permissions and minor fixes

--- parity-aura/all-stop.sh
+++ parity-aura/all-stop.sh
@@ -9,7 +9,7 @@ i=0
 for client in `cat $CLIENTS`; do
     if [[ $i -lt $clientCount ]]; then
         ssh -i $SSH_KEY -oStrictHostKeyChecking=no $USER@$client sudo killall -s KILL node
-        echo stopped driver on client $client
+        echo stopped node on client $client
     fi
     let i=$i+1
 done
