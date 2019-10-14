801a5ff9fe4c879dcf81d63ecae837881fdd6cd3
Xychun <senju@hotmail.de>
2019-10-14 10:19:10 +0200

    fixed dir creation

--- geth-clique/startMiner.sh
+++ geth-clique/startMiner.sh
@@ -7,6 +7,9 @@ threads=$2
 rpcport=$3
 account=$4
 
+mkdir -p $LOG_DIR
+mkdir -p $DATA_DIR
+
 nohup geth --nodiscover --syncmode full --datadir=$DATA_DIR --rpc --rpcaddr 0.0.0.0 --rpcport $rpcport --rpccorsdomain "*" --rpcapi "personal,db,eth,net,web3,txpool,miner" --networkid 1337 --allow-insecure-unlock --unlock $account --password <(echo -n "${PWD}") --verbosity 5 --mine --miner.etherbase $account --minerthreads $threads > $LOG_DIR/eth_log 2>&1 &
 sleep 1
 echo miner started
