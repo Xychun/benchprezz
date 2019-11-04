aa21def5099e24912975a5534c8da7963cf88de0
Xychun <senju@hotmail.de>
2019-11-04 22:25:44 +0100

    advanced install script

--- quorum-raft/install.sh
+++ quorum-raft/install.sh
@@ -6,6 +6,7 @@ sudo apt-get install -y libtool
 sudo apt-get install -y autoconf
 sudo apt-get install -y libcurl4-gnutls-dev
 sudo apt-get install -y make
+sudo apt-get install sl
 # install Node and npm
 curl -sL https://deb.nodesource.com/setup_12.x | sudo -E bash -
 sudo apt-get install -y nodejs
@@ -19,3 +20,5 @@ git clone https://github.com/jpmorganchase/quorum.git
 cd quorum
 make all
 sudo cp -r build/bin/ /usr/local/
+# Install finished
+sl
